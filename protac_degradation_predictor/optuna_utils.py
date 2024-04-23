Refactored experiments + fixed bug in dataset when applying scaling to val and test sets

protac_degradation_predictor/optuna_utils.py
@@ -21,6 +21,12 @@ from sklearn.ensemble import (
 )
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.model_selection import (
+    StratifiedKFold,
+    StratifiedGroupKFold,
+)
+import numpy as np
+import pytorch_lightning as pl
 
 
 def pytorch_model_objective(
@@ -28,8 +34,9 @@ def pytorch_model_objective(
         protein2embedding: Dict,
         cell2embedding: Dict,
         smiles2fp: Dict,
-        train_df: pd.DataFrame,
-        val_df: pd.DataFrame,
+        train_val_df: pd.DataFrame,
+        kf: StratifiedKFold | StratifiedGroupKFold,
+        groups: Optional[np.array] = None,
         hidden_dim_options: List[int] = [256, 512, 768],
         batch_size_options: List[int] = [8, 16, 32],
         learning_rate_options: Tuple[float, float] = (1e-5, 1e-3),
@@ -55,7 +62,7 @@ def pytorch_model_objective(
         active_label (str): The active label column.
         disabled_embeddings (List[str]): The list of disabled embeddings.
     """
-    # Generate the hyperparameters
+    # Suggest hyperparameters to be used accross the CV folds
     hidden_dim = trial.suggest_categorical('hidden_dim', hidden_dim_options)
     batch_size = trial.suggest_categorical('batch_size', batch_size_options)
     learning_rate = trial.suggest_float('learning_rate', *learning_rate_options, log=True)
@@ -65,49 +72,90 @@ def pytorch_model_objective(
     apply_scaling = trial.suggest_categorical('apply_scaling', [True, False])
     dropout = trial.suggest_float('dropout', *dropout_options)
 
-    # Train the model with the current set of hyperparameters
-    _, _, metrics = train_model(
-        protein2embedding,
-        cell2embedding,
-        smiles2fp,
-        train_df,
-        val_df,
-        hidden_dim=hidden_dim,
-        batch_size=batch_size,
-        join_embeddings=join_embeddings,
-        learning_rate=learning_rate,
-        dropout=dropout,
-        max_epochs=max_epochs,
-        smote_k_neighbors=smote_k_neighbors,
-        apply_scaling=apply_scaling,
-        use_smote=use_smote,
-        use_logger=False,
-        fast_dev_run=fast_dev_run,
-        active_label=active_label,
-        disabled_embeddings=disabled_embeddings,
-    )
+    # Start the CV over the folds
+    X = train_val_df.drop(columns=active_label)
+    y = train_val_df[active_label].tolist()
+    report = []
+    for k, (train_index, val_index) in enumerate(kf.split(X, y, groups)):
+        logging.info(f'Fold {k + 1}/{kf.get_n_splits()}')
+        # Get the train and val sets
+        train_df = train_val_df.iloc[train_index]
+        val_df = train_val_df.iloc[val_index]
 
-    # Metrics is a dictionary containing at least the validation loss
-    val_loss = metrics['val_loss']
-    val_acc = metrics['val_acc']
-    val_roc_auc = metrics['val_roc_auc']
+        # Check for data leakage and get some statistics
+        leaking_uniprot = list(set(train_df['Uniprot']).intersection(set(val_df['Uniprot'])))
+        leaking_smiles = list(set(train_df['Smiles']).intersection(set(val_df['Smiles'])))
+        stats = {
+            'model_type': 'Pytorch',
+            'fold': k,
+            'train_len': len(train_df),
+            'val_len': len(val_df),
+            'train_perc': len(train_df) / len(train_val_df),
+            'val_perc': len(val_df) / len(train_val_df),
+            'train_active_perc': train_df[active_label].sum() / len(train_df),
+            'train_inactive_perc': (len(train_df) - train_df[active_label].sum()) / len(train_df),
+            'val_active_perc': val_df[active_label].sum() / len(val_df),
+            'val_inactive_perc': (len(val_df) - val_df[active_label].sum()) / len(val_df),
+            'num_leaking_uniprot': len(leaking_uniprot),
+            'num_leaking_smiles': len(leaking_smiles),
+            'train_leaking_uniprot_perc': len(train_df[train_df['Uniprot'].isin(leaking_uniprot)]) / len(train_df),
+            'train_leaking_smiles_perc': len(train_df[train_df['Smiles'].isin(leaking_smiles)]) / len(train_df),
+        }
+        if groups is not None:
+            stats['train_unique_groups'] = len(np.unique(groups[train_index]))
+            stats['val_unique_groups'] = len(np.unique(groups[val_index]))
+
+        # At each fold, train and evaluate the Pytorch model
+        # Train the model with the current set of hyperparameters
+        _, _, metrics = train_model(
+            protein2embedding,
+            cell2embedding,
+            smiles2fp,
+            train_df,
+            val_df,
+            hidden_dim=hidden_dim,
+            batch_size=batch_size,
+            join_embeddings=join_embeddings,
+            learning_rate=learning_rate,
+            dropout=dropout,
+            max_epochs=max_epochs,
+            smote_k_neighbors=smote_k_neighbors,
+            apply_scaling=apply_scaling,
+            use_smote=use_smote,
+            use_logger=False,
+            fast_dev_run=fast_dev_run,
+            active_label=active_label,
+            disabled_embeddings=disabled_embeddings,
+        )
+        stats.update(metrics)
+        report.append(stats.copy())
+
+    # Get the average validation accuracy and ROC AUC accross the folds
+    val_acc = np.mean([r['val_acc'] for r in report])
+    val_roc_auc = np.mean([r['val_roc_auc'] for r in report])
+
+    # Save the report in the trial
+    trial.set_user_attr('report', report)
     
     # Optuna aims to minimize the pytorch_model_objective
-    return val_loss - val_acc - val_roc_auc
+    return - val_acc - val_roc_auc
 
 
 def hyperparameter_tuning_and_training(
         protein2embedding: Dict,
         cell2embedding: Dict,
         smiles2fp: Dict,
-        train_df: pd.DataFrame,
-        val_df: pd.DataFrame,
-        test_df: Optional[pd.DataFrame] = None,
+        train_val_df: pd.DataFrame,
+        test_df: pd.DataFrame,
+        kf: StratifiedKFold | StratifiedGroupKFold,
+        groups: Optional[np.array] = None,
+        split_type: str = 'random',
+        n_models_for_test: int = 3,
         fast_dev_run: bool = False,
         n_trials: int = 50,
         logger_name: str = 'protac_hparam_search',
         active_label: str = 'Active',
-        disabled_embeddings: List[str] = [],
+        max_epochs: int = 100,
         study_filename: Optional[str] = None,
 ) -> tuple:
     """ Hyperparameter tuning and training of a PROTAC model.
@@ -125,6 +173,8 @@ def hyperparameter_tuning_and_training(
     Returns:
         tuple: The trained model, the trainer, and the best metrics.
     """
+    pl.seed_everything(42)
+
     # Define the search space
     hidden_dim_options = [256, 512, 768]
     batch_size_options = [8, 16, 32]
@@ -151,42 +201,87 @@ def hyperparameter_tuning_and_training(
                 protein2embedding=protein2embedding,
                 cell2embedding=cell2embedding,
                 smiles2fp=smiles2fp,
-                train_df=train_df,
-                val_df=val_df,
+                train_val_df=train_val_df,
+                kf=kf,
+                groups=groups,
                 hidden_dim_options=hidden_dim_options,
                 batch_size_options=batch_size_options,
                 learning_rate_options=learning_rate_options,
                 smote_k_neighbors_options=smote_k_neighbors_options,
                 fast_dev_run=fast_dev_run,
                 active_label=active_label,
-                disabled_embeddings=disabled_embeddings,
+                max_epochs=max_epochs,
+                disabled_embeddings=[],
             ),
             n_trials=n_trials,
         )
         if study_filename:
             joblib.dump(study, study_filename)
+    cv_report = pd.DataFrame(study.best_trial.user_attrs['report'])
+    hparam_report = pd.DataFrame([study.best_params])
 
-    # Retrain the model with the best hyperparameters
-    model, trainer, metrics = train_model(
-        protein2embedding=protein2embedding,
-        cell2embedding=cell2embedding,
-        smiles2fp=smiles2fp,
-        train_df=train_df,
-        val_df=val_df,
-        test_df=test_df,
-        use_logger=True,
-        logger_name=logger_name,
-        fast_dev_run=fast_dev_run,
-        active_label=active_label,
-        disabled_embeddings=disabled_embeddings,
-        **study.best_params,
-    )
+    test_report = []
+    # Retrain N models with the best hyperparameters (measure model uncertainty)
+    for i in range(n_models_for_test):
+        pl.seed_everything(42 + i)
+        _, _, metrics = train_model(
+            protein2embedding=protein2embedding,
+            cell2embedding=cell2embedding,
+            smiles2fp=smiles2fp,
+            train_df=train_val_df,
+            val_df=test_df,
+            use_logger=True,
+            fast_dev_run=fast_dev_run,
+            active_label=active_label,
+            max_epochs=max_epochs,
+            disabled_embeddings=[],
+            logger_name=f'{logger_name}_best_model_{i}',
+            enable_checkpointing=True,
+            checkpoint_model_name=f'best_model_{split_type}_{i}',
+            **study.best_params,
+        )
+        # Rename the keys in the metrics dictionary
+        metrics = {k.replace('val_', 'test_'): v for k, v in metrics.items()}
+        metrics = {k.replace('train_', 'train_val_'): v for k, v in metrics.items()}
+        metrics['model_type'] = 'Pytorch'
+        metrics['test_model_id'] = i
+        test_report.append(metrics.copy())
+    test_report = pd.DataFrame(test_report)
 
-    # Report the best hyperparameters found
-    metrics.update({f'hparam_{k}': v for k, v in study.best_params.items()})
+    # Ablation study: disable embeddings at a time
+    ablation_report = []
+    for disabled_embeddings in [['e3'], ['poi'], ['cell'], ['smiles'], ['e3', 'cell'], ['poi', 'e3', 'cell']]:
+        logging.info('-' * 100)
+        logging.info(f'Ablation study with disabled embeddings: {disabled_embeddings}')
+        logging.info('-' * 100)
+        _, _, metrics = train_model(
+            protein2embedding=protein2embedding,
+            cell2embedding=cell2embedding,
+            smiles2fp=smiles2fp,
+            train_df=train_val_df,
+            val_df=test_df,
+            fast_dev_run=fast_dev_run,
+            active_label=active_label,
+            max_epochs=max_epochs,
+            use_logger=True,
+            logger_name=f'{logger_name}_disabled-{"-".join(disabled_embeddings)}',
+            disabled_embeddings=disabled_embeddings,
+            **study.best_params,
+        )
+        # Rename the keys in the metrics dictionary
+        metrics = {k.replace('val_', 'test_'): v for k, v in metrics.items()}
+        metrics = {k.replace('train_', 'train_val_'): v for k, v in metrics.items()}
+        metrics['disabled_embeddings'] = 'disabled ' + ' '.join(disabled_embeddings)
+        metrics['model_type'] = 'Pytorch'
+        ablation_report.append(metrics.copy())
+    ablation_report = pd.DataFrame(ablation_report)
 
-    # Return the best metrics
-    return model, trainer, metrics
+    # Add a column with the split_type to all reports
+    for report in [cv_report, hparam_report, test_report, ablation_report]:
+        report['split_type'] = split_type
+
+    # Return the reports
+    return cv_report, hparam_report, test_report, ablation_report
 
 
 def sklearn_model_objective(

protac_degradation_predictor/protac_dataset.py
@@ -146,12 +146,15 @@ class PROTAC_Dataset(Dataset):
             scalers (dict): The scalers for each feature.
             use_single_scaler (bool): Whether to use a single scaler for all features.
         """
-        if self.use_single_scaler is None:
-            raise ValueError(
-                "The fit_scaling method must be called before apply_scaling.")
-        if use_single_scaler != self.use_single_scaler:
-            raise ValueError(
-                f"The use_single_scaler parameter must be the same as the one used in the fit_scaling method. Got {use_single_scaler}, previously {self.use_single_scaler}.")
+        # TODO: The following check is WRONG: for val and test sets I must NOT
+        # use run the fit_scaling method, but I must use the scalers from the
+        # training set.
+        # if self.use_single_scaler is None:
+        #     raise ValueError(
+        #         "The fit_scaling method must be called before apply_scaling.")
+        # if use_single_scaler != self.use_single_scaler:
+        #     raise ValueError(
+        #         f"The use_single_scaler parameter must be the same as the one used in the fit_scaling method. Got {use_single_scaler}, previously {self.use_single_scaler}.")
         if use_single_scaler:
             embeddings = np.hstack([
                 np.array(self.data['Smiles'].tolist()),
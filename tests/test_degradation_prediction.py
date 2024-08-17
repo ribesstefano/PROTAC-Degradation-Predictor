91b5b323a82fbc179d1c68c2182e815783bf2bd6
ribesstefano <ribes.stefano@gmail.com>
2024-08-17 21:46:08 +0200

    Uploaded current best models in package code

--- tests/test_degradation_prediction.py
+++ tests/test_degradation_prediction.py
@@ -96,3 +96,22 @@ def test_xgboost_active_proba_list():
 
 def test_is_protac_active():
     pass
+
+def test_active_proba_xgboost():
+    protac_smiles = 'Cc1ncsc1-c1ccc([C@H](C)NC(=O)[C@@H]2C[C@@H](O)CN2C(=O)[C@@H](NC(=O)CC(=O)N2CCN(CC[C@H](CSc3ccccc3)Nc3ccc(S(=O)(=O)NC(=O)c4ccc(N5CCN(CC6=C(c7ccc(Cl)cc7)CCC(C)(C)C6)CC5)cc4)cc3S(=O)(=O)C(F)(F)F)CC2)C(C)(C)C)cc1'
+    e3_ligase = 'VHL'
+    target_uniprot = 'Q07817'
+    cell_line = 'MOLT-4'
+    device = 'cpu'
+
+    active_prob = get_protac_active_proba(
+        protac_smiles=protac_smiles,
+        e3_ligase=e3_ligase,
+        target_uniprot=target_uniprot,
+        cell_line=cell_line,
+        device=device,
+        use_xgboost_models=True,
+        study_type='similarity',
+    )
+
+    print(f'[XGBoost] Active probability: {active_prob} (CPU)')

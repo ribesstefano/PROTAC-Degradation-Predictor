989c91c295aff53dbcba881da243cadc6453e198
ribesstefano <ribes.stefano@gmail.com>
2024-08-19 12:01:46 +0200

    Added all required files for documentation + Updated tests for embeddings

--- tests/test_degradation_prediction.py
+++ tests/test_degradation_prediction.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from protac_degradation_predictor import (
     get_protac_active_proba,
     is_protac_active,
+    get_protac_embedding,
 )
 
 import torch
@@ -115,3 +116,37 @@ def test_active_proba_xgboost():
     )
 
     print(f'[XGBoost] Active probability: {active_prob} (CPU)')
+
+
+def test_embeddings():
+    protac_smiles = 'Cc1ncsc1-c1ccc([C@H](C)NC(=O)[C@@H]2C[C@@H](O)CN2C(=O)[C@@H](NC(=O)CC(=O)N2CCN(CC[C@H](CSc3ccccc3)Nc3ccc(S(=O)(=O)NC(=O)c4ccc(N5CCN(CC6=C(c7ccc(Cl)cc7)CCC(C)(C)C6)CC5)cc4)cc3S(=O)(=O)C(F)(F)F)CC2)C(C)(C)C)cc1'
+    e3_ligase = 'VHL'
+    target_uniprot = 'Q07817'
+    cell_line = 'MOLT-4'
+    device = 'cpu'
+
+    protac_emb = get_protac_embedding(
+        protac_smiles=protac_smiles,
+        e3_ligase=e3_ligase,
+        target_uniprot=target_uniprot,
+        cell_line=cell_line,
+        device=device,
+        study_type='similarity',
+    )
+
+    print(f'Embeddings: {protac_emb}')
+    for model, emb in protac_emb.items():
+        print(f'\t- shape: {emb.shape}')
+
+    protac_emb = get_protac_embedding(
+        protac_smiles=[protac_smiles] * 8,
+        e3_ligase=[e3_ligase] * 8,
+        target_uniprot=[target_uniprot] * 8,
+        cell_line=[cell_line] * 8,
+        device=device,
+        study_type='similarity',
+    )
+
+    print(f'Embeddings (batched): {protac_emb}')
+    for model, emb in protac_emb.items():
+        print(f'\t- shape: {emb.shape}')
